93d615405ee9bd3c2e0159da07d1dfd659f901f1
eskander-taher <eskander.alshaibany@gmail.com>
2024-01-22 23:54:36 +0500

    add landing page layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
 		<MaxWidthWrapper>
 			<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-				hello world
+
+				smu
 			</div>
 		</MaxWidthWrapper>
 	);
