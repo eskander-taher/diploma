c82532ee3f7b661724e82152118ee93d3d99fce4
eskander-taher <eskander.alshaibany@gmail.com>
2024-01-23 23:21:52 +0500

    add vite

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,12 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import RegisterForm from "@/components/RegisterForm";
 
 
 export default function Home() {
 	return (
 		<MaxWidthWrapper>
 			<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-
-				smu
+				<RegisterForm />
 			</div>
 		</MaxWidthWrapper>
 	);
